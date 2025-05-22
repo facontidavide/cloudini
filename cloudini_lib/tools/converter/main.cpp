d072b5c6544b9fa0652ef1d7c35b294322f7ecd4
Davide Faconti <davide.faconti@gmail.com>
2025-05-22 22:51:08 +0200

    working on converter

---
+++ cloudini_lib/tools/converter/main.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QSettings>
+
+#define MCAP_IMPLEMENTATION
+#include "mcap/reader.hpp"
+#include "mcap/writer.hpp"
+
+int main(int argc, char *argv[])
+{
+  QApplication app(argc, argv);
+
+  QCoreApplication::setOrganizationName("Auryn");
+  QCoreApplication::setApplicationName("MCAP_Editor");
+  QSettings::setDefaultFormat(QSettings::IniFormat);
+
+  MainWindow w;
+  w.show();
+  return app.exec();
+}
---
+++ cloudini_lib/tools/converter/mainwindow.h
@@ -0,0 +1,39 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QByteArray>
+
+namespace Ui {
+class MainWindow;
+}
+
+namespace mcap {
+class McapReader;
+}
+
+class MainWindow : public QMainWindow
+{
+  Q_OBJECT
+
+ public:
+  explicit MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
+
+ private slots:
+
+  void on_toolButtonEncode_clicked();
+  void on_toolButtonDecode_clicked();
+  void on_toolButtonLoad_clicked();
+
+ private:
+  void processMCAP(bool encode);
+
+ private:
+  Ui::MainWindow *ui;
+  QString filename_;
+  std::shared_ptr<mcap::McapReader> reader_;
+  QByteArray read_buffer_;
+};
+
+#endif // MAINWINDOW_H
