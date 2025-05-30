1380e679304797e60f04e60e0ce2b84fb3740767
Davide Faconti <davide.faconti@gmail.com>
2025-05-30 13:37:29 +0200

    add license

--- cloudini_lib/include/cloudini_lib/ros_msg_utils.hpp
+++ cloudini_lib/include/cloudini_lib/ros_msg_utils.hpp
@@ -1,41 +1,25 @@
+/*
+ * Copyright 2025 Davide Faconti
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #pragma once
 
 #include "cloudini_lib/cloudini.hpp"
 #include "cloudini_lib/contrib/nanocdr.hpp"
 
 namespace Cloudini {
-
-// sensor_msgs::msg::PointCloud2:
-//
-//     std_msgs/Header header
-//     uint32 height
-//     uint32 width
-//     PointField[] fields
-//     bool    is_bigendian
-//     uint32  point_step
-//     uint32  row_step
-//     uint8[] data
-//     bool is_dense
-//
-// sensor_msgs::msg::PointField:
-//
-//     string name
-//     uint32 offset
-//     uint8  datatype
-//     uint32 count
-//
-// point_cloud_interfaces::msg::CompressedPointCloud2
-//
-//     std_msgs/Header header
-//     uint32 height
-//     uint32 width
-//     PointField[] fields
-//     bool    is_bigendian
-//     uint32  point_step
-//     uint32  row_step
-//     uint8[] compressed_data
-//     bool is_dense
-//     string format
 
 struct RosHeader {
   int32_t stamp_sec = 0;    // seconds
